5e6c2b6f6e4fd810dcfaac33dc69a4be30481f3e
Jonathan Marshall <jmarshall@never.you.mind>
2015-08-17 11:55:03 +1200

    update twoway shiny app

--- shiny/twoway/server.R
+++ shiny/twoway/server.R
@@ -1,5 +1,8 @@
 library(shiny)
 
+y0 = 50
+n  = 20
+
 # computes jitter according to the distribution of y
 dist_jitter <- function(y, jitter=0.2) {
   d <- density(y)
@@ -17,84 +20,60 @@ get_data <- function(y0, a, b, ab, n) {
   r <- matrix(rnorm(n*4),4,n)
   r <- as.numeric(t(r - rowMeans(r)))
 
-  # between-group variation
-  y = y0 + a*A + b*B + ab*A*B
+  # y value
+  y = rep(y0, 4*n)
 
   # jitter for plotting
-  x = A + 2*B
+  x = factor(A + 2*B)
   jitter <- do.call(c, tapply(r, x, dist_jitter, jitter=0.07))
-
   data.frame(y, r, A, B, jitter=jitter)
 }
 
 shinyServer(function(input, output, session) {
 
-  dat <- reactiveValues(data=get_data(max_groups,max_samples))
+  dat <- reactiveValues(data=get_data(y0,0,0,0,n))
 
   observeEvent(input$new_data, {
     # generate some new data
-    cat("new_data hit\n")
-    dat$data <- get_data(max_groups, max_samples)
+    dat$data <- get_data(y0, input$effectA, input$effectB, input$effectAB, n)
   })
 
   val <- reactive({
-    cat("val computed\n")
+    data <- dat$data #get_data(y0, input$effectA, input$effectB, input$effectAB, n)
 
-    data <- dat$data[ get_sample(input$g, input$n),]
-    
     # get rid of unused groups, and normalise residuals per group
-    data <- droplevels(data)
-    data$r <- data$r - rep(tapply(data$r, data$x, mean), each=input$n)
+    x <- data$A + 2*data$B
+    data$r <- data$r - rep(tapply(data$r, x, mean), each=n)
+    data$y <- data$y + input$effectA*data$A + input$effectB*data$B + input$effectAB*data$A*data$B + data$r*input$resid*5
 
-    # compute y
-    data$y <- input$strength * data$g + input$resid * data$r
-
-    print(summary(lm(y ~ x, data=data)))
-    print(anova(lm(y ~ x, data=data)))
-    print(tapply(data$y, data$x, mean))
-    
-    # TODO: Normalise the between-group variation to what we want it to be???
-#    c <- coef(lm(y ~ x))
-#    y <- y - (c[2] - input$strength/50) * x - c[1]
-    list(x=data$x, y=data$y, jitter=data$jitter, lm=lm(y ~ x, data=data))
+    list(x=x+1, y=data$y, jitter=data$jitter, lm1=lm(y ~ A + B, data=data), lm2=lm(y ~ A + B + A:B, data=data))
   })
 
   output$data <- renderPlot({
     # plot the points and model fit
-    cat("data plot rendered\n")
-    par(mar=c(1,0,0,0), cex=2)
-    plot(NULL, xlim=c(0.5, input$g+0.5), ylim=c(-2,2), xaxt="n", yaxt="n", xlab="", ylab="")
+    par(mar=c(3,0,0,0), cex=2)
+    plot(NULL, xlim=c(0.5, 4+0.5), ylim=c(30,70) ,xaxt="n", yaxt="n", xlab="", ylab="")
+    print(val()$x)
+    print(val()$y)
+    segments(-1,seq(30,70,by=2),5,seq(30,70,by=2),"grey80")
     points(as.numeric(val()$x) + val()$jitter, val()$y, col="#00000050", pch=19, xlab="", ylab="", xaxt="n", yaxt="n")
-
+    axis(side=1, at=1:4, labels=c("Control", "Drug A", "Drug B", "Both A&B"))
     # model fit...
-    fit_width <- 0.2
+    fit_width <- 0.1
     ci_width  <- 0.15
-    new_data <- data.frame(x=factor(1:input$g))
-    ci <- predict(val()$lm, new_data, interval="confidence")
-#    rect(1:input$g-ci_width, ci[,2], 1:input$g+ci_width, ci[,3], col="#ff000040", border=NA)
-    segments(1:input$g-fit_width, ci[,1], 1:input$g+fit_width, ci[,1], lwd=3, col="red")
+    new_data <- expand.grid(A=0:1, B=0:1)
+    if (input$interaction) {
+      ci1 <- predict(val()$lm1, new_data, interval="confidence")
+      points(1:4-fit_width, ci1[,1], pch=19, cex=1.5, col="red")
+      segments(1:4-fit_width, ci1[,2], 1:4-fit_width, ci1[,3], lwd=2, col="red")
+      ci2 <- predict(val()$lm2, new_data, interval="confidence")
+      points(1:4+fit_width, ci2[,1], pch=19, cex=1.5, col="blue")
+      segments(1:4+fit_width, ci2[,2], 1:4+fit_width, ci2[,3], lwd=2, col="blue")
+    } else {
+      ci1 <- predict(val()$lm1, new_data, interval="confidence")
+      points(1:4, ci1[,1], pch=19, cex=1.5, col="red")
+      segments(1:4, ci1[,2], 1:4, ci1[,3], lwd=2, col="red")
+    }
   })
 
-  output$fdist <- renderPlot({
-
-    # plot the fdistribution for the null
-    f <- seq(0.1,20,by=0.1)
-    y <- df(f, input$g-1, input$n-input$g)
-    max_y <- 0.5
-
-    x1 <- seq(qf(0.95, input$g-1, input$n-input$g), 20, by=0.1)
-    y1 <- df(x1, input$g-1, input$n-input$g)
-
-    par(mar=c(1,0,0,0), cex=2)
-    plot(NULL, xlim=c(0,20), ylim=c(0,max_y), xaxt="n", yaxt="n", xlab="", ylab="", type="l", xaxs="i", yaxs="i")
-    polygon(c(x1[1], x1, x1[length(x1)]), c(0, y1, 0), col="grey80", border=NA)
-    lines(f, y, lwd=2)
-
-    # mark a line at our f-statistic
-    fhat <- summary(val()$lm)$fstatistic[1]
-    abline(v=fhat, col="red", lwd=2)
-    text(19, max_y*0.95, bquote(F == .(round(fhat,2))), adj=c(1,1), col="red")
-    text(19, max_y*0.85, bquote(P == .(sprintf("%0.4f", 1-pf(fhat, input$g-1, input$n-input$g)))), adj=c(1,1), col="grey60")
-    text(19, 0.03, "F distribution", col="grey80", adj=c(1,0))
-  })
 })
